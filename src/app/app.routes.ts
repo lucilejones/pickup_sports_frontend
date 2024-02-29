8e7fe936758ed11a691dab83ce50b48e573d5dc9
Lucile Jones <jones.lucile@gmail.com>
2024-02-28 20:36:03 -0700

    added login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     {
         path: 'events',
         loadComponent: () => import('./features/events/events.component').then((c) => c.EventsComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent)
     }
 ];
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiIrl: 'http://localhost:3000'
+    apiUrl: 'http://localhost:3000'
 };
